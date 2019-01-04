00fd1f5bb1c732127c29f32cd200c3334fc1f389
whynot-s <whynot_s@outlook.com>
2019-01-04 10:51:34 +0000

    label count

--- Label.py
+++ Label.py
@@ -17,6 +17,28 @@ mysql_db = pymysql.connect(mysqlConfig['host'], mysqlConfig['username'], mysqlCo
 cursor = mysql_db.cursor()
 print("Connected to Mysql %s:%s %s" % (mysqlConfig['host'], mysqlConfig['port'], mysqlConfig['db']))
 
-results = readme_cleaned.find({}, {'keyword' : 1}).limit(20)
+results = readme_cleaned.find({}, {'keyword' : 1, '_id' : 0})
+numbers = {}
+total = len(results)
+i = 0
 for result in results:
-    print(result)
+    i += 1
+    keywords = result['keyword']
+    for keyword in keywords:
+        temp = numbers[keywords]
+        if not temp:
+            numbers[keywords] = 1
+        else:
+            numbers[keywords] = temp + 1
+    if i % 1000 == 0:
+        print("PROCESS: %s/%s", i, total)
+
+i = 0
+for key, value in numbers.items():
+    i += 1
+    cursor.execute("INSERT INTO Labels VALUES(\"%s\", %s)" % (key, value))
+    if i % 1000 == 0:
+        print("INSERT: %s/%s", i, total)
+
+mysql_db.commit()
+
