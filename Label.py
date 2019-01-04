0ff6aa144f8f9fafe0c61ee51fbc3d661145b83b
whynot-s <whynot_s@outlook.com>
2019-01-04 12:14:46 +0000

    label count fix bugs

--- Label.py
+++ Label.py
@@ -9,35 +9,35 @@ with open("dbConfig.json", "r") as f:
 mongoConfig = dbConfig["mongodb"]
 client = MongoClient(mongoConfig["host"], mongoConfig["port"])
 db = client.github
-readme_cleaned = db.readme_cleaned
-print("Connected to Mongodb %s:%s %s" % (mongoConfig["host"], mongoConfig["port"], "readme_cleaned"))
+topics = db.topics
+print("Connected to Mongodb %s:%s %s" % (mongoConfig["host"], mongoConfig["port"], "topics"))
 
 mysqlConfig = dbConfig["mysql"]
 mysql_db = pymysql.connect(mysqlConfig['host'], mysqlConfig['username'], mysqlConfig['password'], mysqlConfig['db'])
 cursor = mysql_db.cursor()
 print("Connected to Mysql %s:%s %s" % (mysqlConfig['host'], mysqlConfig['port'], mysqlConfig['db']))
 
-results = readme_cleaned.find({}, {'keyword' : 1, '_id' : 0})
+results = topics.find({}, {'topic' : 1, '_id' : 0})
 numbers = {}
 
 i = 0
 for result in results:
     i += 1
-    keywords = result['keyword']
-    for keyword in keywords:
-        if keyword in numbers:
-            numbers[keyword] += 1
+    topics = result['topic']
+    for topic in topics:
+        if topic in numbers:
+            numbers[topic] += 1
         else:
-            numbers[keyword] = 1
+            numbers[topic] = 1
     if i % 1000 == 0:
-        print("PROCESS: %s", i)
+        print("PROCESS: %s" % i)
 
 j = 0
 for key, value in numbers.items():
     j += 1
     cursor.execute("INSERT INTO Labels VALUES(\"%s\", %s)" % (key, value))
     if j % 1000 == 0:
-        print("INSERT: %s/%s", j, i)
+        print("INSERT: %s/%s" % (j, i))
 
 mysql_db.commit()
 
